ce252ebd73ea3e70e51429637470f93aa5e7bafa
Kelly de Vries <kdv@me.com>
2015-01-28 19:54:36 -0800

    more cleaning

---
+++ K_portfolio_resume/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Kelly's Portfolio</title>
+    <link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen">
+</head>
+<body>
+  <div class="container clearfix">
+    <div class="grid_4 omega">  <img src="img/headshot.jpg" alt="Kelly de Vries">
+  <ul class="nav">
+    <li><a href="#">Social Networks</a></li>
+    <li class="last"><a href="#">Contact</a></li>
+  </ul>
+  </div>
+  <div id="intro">
+    <h1>Always Asking. Always Learning.
+    </h1>
+    <p><a href="#" class="btn">Learn more about me.</a></p>
+  </div>
+  <img src="img/you-bake-me-blush.gif" alt="You Bake Me Blush">
+  <div id="featured-cupcake">
+    <h2>Education</h2>
+    <ul class="schools">
+      <li>George Fox University</li>
+      <li>Drexel University</li>
+      <li>Linfield College</li>
+    </ul>
+    <img src="img/featured-cupcake.jpg" alt="Avocado Chocolate Cupcake">
+  </div>
+
+  <div id="new-cupcakes">
+    <h2>Experience</h2>
+    <p>Our newest cupcakes are <a href="#">Bacon Me Crazy</a> and <a href="#">Jalapeño So Spicy</a>.</p>
+    <img src="img/new-cupcake-bacon.jpg" alt="Bacon Me Crazy">
+    <img src="img/new-cupcake-jalapeno.jpg" alt="Jalapeño So Spicy">
+  </div>
+
+  <h2>Inside the Kitchen</h2>
+  <p>Smells Like Bakin' started out in the garage of the husband wife duo Allison &amp; Joseph.  Allison is the baker, and Joseph found a way for then to make a business out of her tasty treats.  Flash forward to today and they have a successful store front, catering business and cupcake truck.</p>
+  <p><a href="#" class="btn-small">Read More</a></p>
+
+  <h2>Get Bakin' with Us</h2>
+
+  <div id="contact">
+    <p>Call us: <span>555-CUP-CAKE</span><br>
+    Email us: <a href="#">bakeon@smellslikebakin.com</a></p>
+  </div>
+
+  <p>We announce all of our new flavors first though Facebook &amp; Twitter, and even take requests!</p>
+  <a href="https://www.facebook.com/kelly.woodsdevries"><img src="img/facebook.png" alt-"Facebook"></a>
+  <a href="https://twitter.com/kdv24"><img src="img/twitter.png" alt-"Twitter"></a>
+  <a href="kdv.pdx@gmail.com"><img src="img/email.png" alt-"Email"></a>
+
+
+  <div id="copyright">
+  <p>&copy; 2014 Kelly de Vries. All Rights Reserved.</p>
+  </div>
+
+
+</body>
+</html
---
+++ Kelly_de_Vries_portfolio_site/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link href="css/styles.css" rel="stylesheet" type="text/css" media="all">
+	<title>Kelly de Vries</title>
+	</head>
+
+	<body>
+		<h1 class="heading">Kelly de Vries</h1>
+		<div class="contact">
+			<p><a href="tel:5415390439">541.539.0439</a></p>
+
+			<img src="img/ks_glasses.png"height="40" width="122">
+		</div>
+
+		<p class="final">The content of the document.  Here I will include all kinds of incredibly vital and hopefully witty and engaging info to lure in my prospective employers!  Now I'm trying out <span class="testing">span</span> so I can see how to make fancy pants little changes within other stuff.</p>
+	</body>
+
+</html>
